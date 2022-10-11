417afbcc6bbb63d04a6440ead3dffc4de17c4195
serhii kan <skan@one.kz>
2022-10-11 18:30:53 +0600

    added lesson test

--- src/test/java/org/skan/SelenideFileTest.java
+++ src/test/java/org/skan/SelenideFileTest.java
@@ -2,12 +2,31 @@ package org.skan;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SelenideFileTest {
 
     @Test
-    void selenideDownloadFile(){
+    void selenideDownloadFile() throws Exception {
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File downloadedFile = $("#raw-url").download();
+        try(InputStream is = new FileInputStream(downloadedFile)) {
+            byte[] fileSource = is.readAllBytes();
+            String fileContent = new String(fileSource, StandardCharsets.UTF_8);
+            assertThat(fileContent).contains("This repository is the home of the next generation of JUnit, _JUnit 5_.");
+        }
+    }
 
-
+    @Test
+    void uploadFile() throws Exception{
 
     }
 }
